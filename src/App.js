62d35f884ac30fd0a0d83d4c2564016cc5e65d2d
Shubham1322c <shubhamoffical1322@gmail.com>
2023-07-30 17:37:33 +0530

    Project Complete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import propTypes from "prop-types";
-import React from 'react'
+import React , { useState } from 'react';
 
 
 function App() {
+const [mode, setMode] = useState('light');
+
+const toggleMode = () => {
+  if(mode === "light"){
+    setMode('dark');
+    document.body.style.backgroundColor = "black"
+  }else{
+    setMode('light');
+    document.body.style.backgroundColor = "white"
+  }
+
+}
 
   return (
     <>
 
-    <Navbar/>
+    <Navbar mode={mode} toggleMode={toggleMode}/>
 
     <div className="container">
-      <TextForm/>
+      <TextForm mode={mode} toggleMode={toggleMode}/>
     </div>
 
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,8 +14,14 @@ export default function TextForm(props) {
   };
 
   const handleSpace = () => {
-    let ClearSpace = text.trim();
-    setText(ClearSpace);
+    let ClearSpace = text.split(/[ ]+/);
+    setText(ClearSpace.join(" "));
+  };
+
+  const handleCopy = () => {
+    let text = document.getElementById("myText");
+    text.select();
+    navigator.clipboard.writeText(text.value);
   };
 
   const handleClear = () => {
@@ -27,34 +33,39 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
-  let bg = document.body.style.cssText === `background-color:rgb(53, 55, 64); color: whitesmoke; `;
+  // let bg = document.body.style.cssText === `background-color:rgb(53, 55, 64); color: whitesmoke; `;
 
-  if(bg){
-    document.getElementById("myText").style.cssText = `background-color: #6e6e80;
-    outline: none;
-    border: none;`;
+  // if(bg){
+  //   document.getElementById("myText").style.cssText = `background-color: #6e6e80;
+  //   outline: none;
+  //   border: none;`;
 
-  }else if(bg){
-    document.body.style = `background-color:#353740; color: whitesmoke;`;
+  // }else if(bg){
+  //   document.body.style = `background-color:#353740; color: whitesmoke;`;
     
-  }
+  // }
 
   return (
     <>
-      <div className="mb-3 my-4">
+    <div className={`container text-${props.mode==='light'?'dark':'light'}`}>
+
+   
+      <div className={`mb-3 my-4`}>
         <h1>{props.heading} &#128071;</h1>
         <textarea className="form-control mb-3" id="myText" rows="8" value={text} onChange={handleOnChange}></textarea>
         <button className="btn btn-primary mx-1 my-1" onClick={handleUpbtn}>Convert To Uppercase</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleLowbtn}>Convert To Lowercase</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleSpace}>Clear Extraspace</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy To Clipboard</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleClear}>Clear</button>
       </div>
       <div className="Info">
         <h3>Text Information &#10024;</h3>
         <p>{text.split(".").length - 1} Sentence {text.split(" ").length - 1} Word and {text.length} Characters</p>
         <p>{0.18 * text.split(" ").length - 0.18} Minutes To Read</p>
-        <h3>Preview &#128302;</h3>
+        <h3>{text===""?"Enter Your Text In The Text Are To Preview Here!":'Preview'}&#128302;</h3>
         <p>{text}</p>
+      </div>
       </div>
     </>
   );
